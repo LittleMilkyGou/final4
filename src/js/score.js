5ce477ac5c39ddad51a754514b62a5096585be98
LittleMilkyGou <92155580+LittleMilkyGou@users.noreply.github.com>
2021-10-28 12:30:55 +1000

    Add files via upload

---
+++ src/js/score.js
@@ -0,0 +1,30 @@
+let mark =localStorage.getItem("score");
+var score = mark + 2;
+var username=localStorage.getItem("username"); //get current user's information
+
+function upload(){
+    $.ajax({
+        method: "post",
+        url : "php/score.php",
+        data : {
+            score : score,
+            username : username,
+            
+        },
+        success : function(result){
+            var res = JSON.parse(result); //get result back from php
+            if (res.code==0){
+                alert("fail to connect database");
+            } else {
+                confirm("upload mark successful");
+                setTimeout(function(){
+                    window.location.href = "simulation.html";
+                },600);
+            }
+                    
+        },
+        error : function(msg){
+            console.log(msg);
+        }
+    })
+}
